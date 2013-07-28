2ab67c9e72ceedc31b6a7185c05de9d2777d602e
Eric Hogue <eric@erichogue.ca>
2013-07-27 20:56:14 -0400

    Use https to clone the repo

--- scripts/cloneRepository.php
+++ scripts/cloneRepository.php
@@ -6,22 +6,24 @@ $repositoryToClone = array('joind.in', 'joindin-api', 'joindin-web2');
 $path = realpath(__DIR__ . '/../');
 chdir($path);
 
-$gitUsername = getGitUsername();
+$remoteString = `git remote show origin`;
+$useHttps = isHttpsClone($remoteString);
 
-array_walk($repositoryToClone, 'cloneRepository', $gitUsername);
-array_walk($repositoryToClone, 'addUpstreamRemote');
+$gitUsername = getGitUsername($remoteString, $useHttps);
 
-function getGitUsername() {
-	$remote = `git remote show origin`;
-	$isHttps = isHttpsClone($remote);
+array_walk($repositoryToClone, 'cloneRepository', array($gitUsername, $useHttps));
+array_walk($repositoryToClone, 'addUpstreamRemote', $useHttps);
 
-	if ($isHttps) {
+
+function getGitUsername($remoteString, $useHttps) {
+
+	if ($useHttps) {
 		$pattern = '/\s*Fetch URL:\s+https:\/\/github\.com\/(.+)\//';
 	} else {
 		$pattern = '/\s*Fetch URL:\s+git@github\.com:(.+)\//';
 	}
 
-	$found = preg_match($pattern, $remote, $matches);
+	$found = preg_match($pattern, $remoteString, $matches);
 	if (!$found || 2 != count($matches)) {
 		die('Failed to extract the username from git origin');
 	}
@@ -29,16 +31,33 @@ function getGitUsername() {
 	return $matches[1];
 }
 
-function cloneRepository($repoName, $index, $gitUsername) {
+function cloneRepository($repoName, $index, $values) {
+	$gitUsername = $values[0];
+	$useHttps = $values[1];
+
 	echo "Cloning {$repoName}\n";
-	$cloneCommand = "git clone git@github.com:{$gitUsername}/{$repoName}.git";
+
+	if ($useHttps) {
+		$cloneCommand = "git clone https://github.com/{$gitUsername}/{$repoName}.git";
+	} else {
+		$cloneCommand = "git clone git@github.com:{$gitUsername}/{$repoName}.git";
+	}
+
+	echo $cloneCommand . "\n";
 	system($cloneCommand);
 }
 
-function addUpstreamRemote($repoName) {
+function addUpstreamRemote($repoName, $index, $useHttps) {
 	echo "Adding upstream remote to {$repoName}\n";
 	chdir($repoName);
-	$remoteCommand = "git remote add upstream git@github.com:joindin/{$repoName}.git";
+
+	if ($useHttps) {
+		$remoteCommand = "git remote add upstream https://github.com/joindin/{$repoName}.git";
+	} else {
+		$remoteCommand = "git remote add upstream git@github.com:joindin/{$repoName}.git";
+	}
+
+	echo $remoteCommand. "\n";
 	system($remoteCommand);
 	chdir('../');
 }
