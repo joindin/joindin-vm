df4fa77f01ff98a793242ef84ca66730769fb004
Rob Allen <rob@akrabat.com>
2014-09-13 21:12:09 +0100

    Update the mailcatcher manifest

--- puppet/modules/joindin/manifests/mailcatcher.pp
+++ puppet/modules/joindin/manifests/mailcatcher.pp
@@ -1,41 +1,48 @@
 class joindin::mailcatcher {
-    package { 'gcc-c++':
+    package { 'gcc':
         ensure   => 'installed'
     }
-    package { 'ruby-devel':
+    package { 'ruby-dev':
         ensure   => 'installed'
     }
-    package { 'sqlite-devel':
+    package { 'libsqlite3-dev':
         ensure   => 'installed'
     }
     package { 'mailcatcher':
         ensure   => 'installed',
         provider => 'gem',
-        require  => Package['ruby-devel', 'gcc-c++', 'sqlite-devel'],
+        require  => Package['ruby-dev', 'gcc', 'libsqlite3-dev'],
     }
     file { "mailcatcher" :
+        ensure => 'present',
         path   => "/etc/init.d/mailcatcher",
         source => "puppet:///modules/joindin/mailcatcher",
         owner  => "root",
         group  => "root",
+        require  => Package['mailcatcher'],
+    }
+    file { "mailcatcher-php.ini" :
+        ensure => 'present',
+        path   => "/etc/php5/mods-available/mailcatcher.ini",
+        source => "puppet:///modules/joindin/mailcatcher.ini",
+        owner  => "root",
+        group  => "root",
+        require => [Package['php']]
     }
     service { 'mailcatcher':
         ensure => 'running',
         enable => true,
         require => [Package['mailcatcher'], File['mailcatcher'], File['mailcatcher-php.ini']]
     }
-    file { "mailcatcher-php.ini" :
-        path   => "/etc/php.d/mailcatcher.ini",
-        source => "puppet:///modules/joindin/mailcatcher.ini",
-        owner  => "root",
-        group  => "root",
-        require => [Package['php']]
+
+    exec { 'mailcatcher-run-on-boot':
+        creates => '/tmp/.mailcatcher-update-rc.d',
+        command => "update-rc.d mailcatcher defaults",
+        require => Service['mailcatcher'],
     }
 
-
-
-    # Remove posfix to ensure that the vm will never send an email by mistake
-    service { 'postfix': 
+    # Remove exim4 to ensure that the vm will never send an email by mistake
+    service { 'exim4':
         ensure => 'stopped',
         enable => false,
     }
