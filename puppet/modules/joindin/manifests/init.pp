f2bb7fbe57a09e3ec49909240d7f0a7945f4d086
Rob Allen <rob@akrabat.com>
2014-09-13 21:01:59 +0100

    Remove setup and add dotdeb repository.

--- puppet/modules/joindin/manifests/init.pp
+++ puppet/modules/joindin/manifests/init.pp
@@ -1,6 +1,19 @@
 # 'site' module.
 class joindin {
-    include joindin::setup
+
+    # add dotdeb repository
+    include joindin::dotdeb
+
+    Exec["update-apt"] -> Package <| |>
+
+
+    # Install some default packages
+    $default_packages = [ "strace", "sysstat", "git", "vim"]
+    package { $default_packages :
+        ensure => present,
+        require => Exec['update-apt'],
+    }
+    
     include joindin::sql
     include joindin::web
     include joindin::app
