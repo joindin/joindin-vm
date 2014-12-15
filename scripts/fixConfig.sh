994a373904cdf4b16b863e699ec8eed4b08b056d
Rob Allen <rob@akrabat.com>
2014-12-15 17:53:45 +0000

    Change references to https://m.joind.in to http://web2.dev.joind.in in api's config file

--- scripts/fixConfig.sh
+++ scripts/fixConfig.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sed -i 's/https:\/\/m.joind.in/http:\/\/web2.dev.joind.in/' /vagrant/joindin-api/src/config.php
 sed -i 's/https:\/\/api.joind.in/http:\/\/api.dev.joind.in/' /vagrant/joindin-web2/config/config.php
 sed -i 's/https:\/\/joind.in\//http:\/\/dev.joind.in\//' /vagrant/joind.in/src/system/application/config/config.php
 sed -i 's/https:\/\/api.joind.in\//http:\/\/api.dev.joind.in\//' /vagrant/joind.in/src/system/application/config/config.php
