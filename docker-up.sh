159187bba7460efdc38734d351a4eb5e9f18495a
Scott Dutton <scott@exussum.co.uk>
2018-07-19 21:01:43 +0100

    Fix db gen issue

---
+++ docker-up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker-compose up -d
+sleep 10;
+docker-compose exec db bash /var/joindin/scripts/patchdb.sh -t /var/joindin -d joindin -u root -p joindin -i
+docker-compose exec api php tools/dbgen/generate.php | docker-compose exec db mysql -u root -pjoindin joindin
+
