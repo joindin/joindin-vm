7cc4d7319703405a943c93d20f01e2d9f67181da
Scott Dutton <scott@exussum.co.uk>
2018-07-19 21:11:21 +0100

    Fix for TTY

--- docker-up.sh
+++ docker-up.sh
@@ -2,5 +2,5 @@
 docker-compose up -d
 sleep 10;
 docker-compose exec db bash /var/joindin/scripts/patchdb.sh -t /var/joindin -d joindin -u root -p joindin -i
-docker-compose exec api php tools/dbgen/generate.php | docker-compose exec db mysql -u root -pjoindin joindin
+docker-compose exec api php tools/dbgen/generate.php | docker-compose exec -T db mysql -u root -pjoindin joindin
 
