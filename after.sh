588cb336c5a5da93b48d5bd9ed3409084a2426e6
Eric Poe <eric.poe@gmail.com>
2019-02-24 20:50:45 -0600

    Prepare working Homestead VM for Joindin

---
+++ after.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# If you would like to do some extra provisioning you may
+# add any commands you wish to this file and they will
+# be run after the Homestead machine is provisioned.
+#
+# If you have user-specific configurations you would like
+# to apply, you may also create user-customizations.sh,
+# which will be run after this script.
+
+# Run migrations
+~/code/joindin-api/scripts/patchdb.sh -t ~/code/joindin-api -d joindin -u homestead -p secret -i
+
+# Import dev DB
+php ~/code/joindin-api/tools/dbgen/generate.php | mysql -u homestead -psecret joindin
