c70aa3f13ff01f42517844e4d983ff0f2b9392ca
Joe Ferguson <joe@joeferguson.me>
2019-02-25 11:16:17 -0600

    🚧 Flesh out more Homestead configuration. Add joindin mysql user, update paths

--- after.sh
+++ after.sh
@@ -8,11 +8,16 @@
 # to apply, you may also create user-customizations.sh,
 # which will be run after this script.
 
+# Create joindin user w/ password
+mysql --user="root" --password="secret" -e "CREATE USER 'joindin'@'0.0.0.0' IDENTIFIED BY 'password';"
+mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'joindin'@'0.0.0.0' IDENTIFIED BY 'password' WITH GRANT OPTION;"
+mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'joindin'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
+
 # Run migrations
-~/code/joindin-api/scripts/patchdb.sh -t ~/code/joindin-api -d joindin -u homestead -p secret -i
+~/joindin/joindin-api/scripts/patchdb.sh -t ~/joindin/joindin-api -d joindin -u homestead -p secret -i
 
 # Import dev DB
-php ~/code/joindin-api/tools/dbgen/generate.php | mysql -u homestead -psecret joindin
+php ~/joindin/joindin-api/tools/dbgen/generate.php | mysql -u homestead -psecret joindin
 
 ## Install server applications
 
