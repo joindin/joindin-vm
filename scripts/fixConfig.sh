3ec3c9617620fd28a25e3a7a524ef932798e043f
Eric Poe <eric.poe@gmail.com>
2019-03-17 23:27:18 -0500

    Remove legacy from fixConfig script

--- scripts/fixConfig.sh
+++ scripts/fixConfig.sh
@@ -2,7 +2,4 @@
 
 sed -i 's/https:\/\/m.joind.in/http:\/\/dev.joind.in/' /vagrant/joindin-api/src/config.php
 sed -i 's/https:\/\/api.joind.in/http:\/\/api.dev.joind.in/' /vagrant/joindin-web2/config/config.php
-sed -i 's/https:\/\/joind.in\//http:\/\/legacy.joind.in\//' /vagrant/joindin-legacy/src/system/application/config/config.php
-sed -i 's/https:\/\/api.joind.in\//http:\/\/api.dev.joind.in\//' /vagrant/joindin-legacy/src/system/application/config/config.php
-sed -i "s/^\$config\['token_dir'].*/\$config\['token_dir'\] = '\/tmp\/ctokens';/" /vagrant/joindin-legacy/src/system/application/config/config.php
 
