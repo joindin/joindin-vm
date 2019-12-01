774180bffe3e92ce4589fd7ab6b42bf0add187de
Joe Ferguson <joe@joeferguson.me>
2019-12-01 10:28:35 -0600

    Upgrade Homestead, keep defaults to PHP 7.3

--- after.sh
+++ after.sh
@@ -19,11 +19,7 @@ mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'joindin'@'%' ID
 # Import dev DB
 php ~/joindin-vm/joindin-api/tools/dbgen/generate.php | mysql -u homestead -psecret joindin
 
-## Install server applications
-
-# Install Wireshark
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y \
-    -o Dpkg::Options::="--force-confdef" \
-    -o Dpkg::Options::="--force-confold" \
-    install tshark -y
+# Set PHP 7.3 on the CLI
+sudo update-alternatives --set php /usr/bin/php7.3
+sudo update-alternatives --set php-config /usr/bin/php-config7.3
+sudo update-alternatives --set phpize /usr/bin/phpize7.3
